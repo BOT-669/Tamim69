1989bb5fc3f3e6952692d9291f5a81dd1e63fe7b
BOT-669 <chowdhorytamim468@gmail.com>
2024-11-09 06:37:27 +0600

    Update info.js

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -28,15 +28,15 @@ var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
 
 var callback = () => api.sendMessage({body:`
 --------------------------------------------
-𝐍𝐚𝐦𝐞       : 𝐉𝐔𝐁𝐀𝐘𝐄𝐑 𝐀𝐇𝐌𝐄𝐃
-𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧   : 𝐈𝐬𝐥𝐚𝐦
-𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝐉𝐀𝐒𝐒𝐎𝐑𝐄
-𝐆𝐞𝐧𝐝𝐞𝐫.   : 𝐌𝐚𝐥𝐞
-𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 : 𝐒𝐢𝐧𝐠𝐥𝐞
-𝐖𝐨𝐫𝐤        : 𝐒𝐭𝐮𝐝𝐞𝐧𝐭
+𝐍𝐚𝐦𝐞       : 𝚃𝙰𝙼𝙸𝙼 𝚇𝙷𝙾𝚆𝙳𝙷𝚄𝚁𝚈
+𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧   : 𝙸𝚂𝙻𝙰𝙼
+𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝚁𝙰𝙹𝚂𝙷𝙰𝚂𝙷𝙸
+𝐆𝐞𝐧𝐝𝐞𝐫.   : 𝙼𝙰𝙻𝙴
+𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 : 𝚂𝙸𝙽𝙶𝙴𝙻
+𝐖𝐨𝐫𝐤        : 𝚂𝚃𝚄𝙳𝙴𝙽𝚃
 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: 𝐈𝐧𝐛𝐨𝐱
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/www.xnxx.chrome`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/itsmetamim1.0 `,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
   fs.unlinkSync(__dirname + "/cache/1.png"));  
-    return request(encodeURI(`https://graph.facebook.com/100092747803433/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
+    return request(encodeURI(`https://graph.facebook.com/100076339585458/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
  };
